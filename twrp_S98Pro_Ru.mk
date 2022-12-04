1c9ad998d6f75fa4a4ae7ccd4bafefcdf9cfb135
spratay <spratay@linuxmail.com>
2022-12-04 15:51:16 +0300

    okey

--- twrp_S98Pro_Ru.mk
+++ twrp_S98Pro_Ru.mk
@@ -1,44 +1,11 @@
-# limitations under the License.
-#
-#
-# Only the below variable(s) need to be changed!
-#
-# Define hardware platform
-#PRODUCT_PLATFORM := kona
+$(call inherit-product, vendor/twrp/config/common.mk)
 
-# Identifier for SoC folder
-#COMMON_SOC := sm8250
-
-#
-#
-#
-# The below variables will be generated automatically
-#
-#
-# Release name (automatically taken from this file's suffix)
-PRODUCT_RELEASE_NAME := $(lastword $(subst /, ,$(lastword $(subst _, ,$(firstword $(subst ., ,$(MAKEFILE_LIST)))))))
-
-# Custom vendor used in build tree (automatically taken from this file's prefix)
-CUSTOM_VENDOR := $(lastword $(subst /, ,$(firstword $(subst _, ,$(firstword $(MAKEFILE_LIST))))))
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/$(CUSTOM_VENDOR)/config/common.mk)
-
-# OEM Info (automatically taken from device tree path)
-BOARD_VENDOR := $(or $(word 2,$(subst /, ,$(firstword $(MAKEFILE_LIST)))),$(value 2))
+# Device specific configs
+$(call inherit-product, device/doogee/S98Pro/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := $(PRODUCT_RELEASE_NAME)
-PRODUCT_NAME := $(CUSTOM_VENDOR)_$(PRODUCT_DEVICE)
-PRODUCT_BRAND := $(BOARD_VENDOR)
-PRODUCT_MODEL := $(shell echo $(PRODUCT_BRAND) | tr  '[:lower:]' '[:upper:]')_$(PRODUCT_DEVICE)
-PRODUCT_MANUFACTURER := $(PRODUCT_BRAND)
-
-# Common path for device trees
-COMMON_PATH := device/$(PRODUCT_BRAND)/$(COMMON_SOC)-common
-
-# Device path for OEM device tree
-DEVICE_PATH := device/$(PRODUCT_BRAND)/$(PRODUCT_DEVICE)
-
-# Inherit from hardware-specific part of the product configuration
-$(call inherit-product, $(DEVICE_PATH)/device.mk)
+PRODUCT_DEVICE := S98Pro_Ru
+PRODUCT_NAME := twrp_S98Pro_Ru
+PRODUCT_BRAND := DOOGEE
+PRODUCT_MODEL := S98Pro_Ru
+PRODUCT_MANUFACTURER := doogee
